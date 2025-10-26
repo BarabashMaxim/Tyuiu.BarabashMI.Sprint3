69fbf9b6e6e643ed36f90233eda7918a62616b79
BarabashMaxim <Tyuiu.BarabashMI@outlook.com>
2025-10-26 16:31:05 +0500

    Task7

---
+++ Tyuiu.BarabashMI.Sprint3.Task7.V25.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.BarabashMI.Sprint3.Task7.V25.Lib
+{
+    public class DataService : ISprint3Task7V25
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            int len = stopValue - startValue + 1;
+            double[] mass = new double[len];
+            int count = 0;
+            for (int i = startValue; i <= stopValue; i++)
+            {
+                mass[count] = Math.Round(Math.Cos(i) + 2 * i - Math.Sin(i) * 3 * i, 2);
+                count++;
+            }
+            return mass;
+
+        }
+    }
+}
---
+++ Tyuiu.BarabashMI.Sprint3.Task7.V25.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.BarabashMI.Sprint3.Task7.V25.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
