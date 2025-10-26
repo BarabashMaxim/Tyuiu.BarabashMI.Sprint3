75dbdf14c400d9beca156af020bee38a40b33cf4
BarabashMaxim <Tyuiu.BarabashMI@outlook.com>
2025-10-26 13:20:40 +0500

    Task4

---
+++ Tyuiu.BarabashMI.Sprint3.Task4.V21.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.BarabashMI.Sprint3.Task4.V21.Lib
+{
+    public class DataService : ISprint3Task4V21
+    {
+        public double Calculate(int startValue, int stopValue)
+        {
+            double res = 1;
+            for (startValue = -5;  startValue < stopValue; startValue++)
+            {
+                if (startValue == 0)
+                {
+                    continue;
+                }
+                res *= (Math.Cos(startValue) - startValue) / startValue;
+            }
+            res = Math.Round(res,3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.BarabashMI.Sprint3.Task4.V21.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.BarabashMI.Sprint3.Task4.V21.Lib;
+namespace Tyuiu.BarabashMI.Sprint3.Task4.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double wait = -0.621;
+            double res = ds.Calculate(-5, 5);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
