3853eee68bbe37bfa06bcea2e81b6ebfe636b0a1
BarabashMaxim <Tyuiu.BarabashMI@outlook.com>
2025-10-26 11:53:34 +0500

    Task3

---
+++ Tyuiu.BarabashMI.Sprint3.Task3.V10.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.BarabashMI.Sprint3.Task3.V10.Lib
+{
+    public class DataService : ISprint3Task3V10
+    {
+        public string DeleteCharInString(string value, char item)
+        {
+            string clinnedString = "";
+            foreach (char c in value)
+            {
+                if (c != 'p')
+                {
+                    clinnedString += c;
+                }
+            }
+            return clinnedString;
+        }
+    }
+}
---
+++ Tyuiu.BarabashMI.Sprint3.Task3.V10.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.BarabashMI.Sprint3.Task3.V10.Lib;
+
+namespace Tyuiu.BarabashMI.Sprint3.Task3.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string wait = "gdff vft ";
+            string res = ds.DeleteCharInString("gdfppf vfppt p", 'p');
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
